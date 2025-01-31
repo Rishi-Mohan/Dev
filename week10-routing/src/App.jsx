a342d22240c9aaeeefca01cd72842b529956907c
Rishi-Mohan <jharishimohan@gmail.com>
2025-02-01 00:47:41 +0530

    rolling up the state: Bulb case

--- week10-routing/src/App.jsx
+++ week10-routing/src/App.jsx
@@ -1,81 +1,63 @@
-import { useRef, useState } from "react";
+// toggle the bulb
+
+import { useState } from "react";
 
 function App() {
-  const [currentCount, setCurrentCount] = useState(0);
-  // const [timer, setTimer] = useState(0);
-  const timerRef = useRef();
-  function startClock() {
-    let val = setInterval(function () {
-      setCurrentCount((c) => c + 1);
-    }, 1000);
-    timerRef.current = val;
-    // setTimer(val);
-  }
-
-  function stopClock() {
-    clearInterval(timerRef.current);
-    // clearInterval(timer);
-  }
-
   return (
     <div>
-      {currentCount}
-      <br />
-      <button onClick={startClock}>Start</button>
-      <button ref={timerRef} onClick={stopClock}>
-        Stop
-      </button>
+      <LightBulb />
     </div>
   );
 }
 
+const LightBulb = () => {
+  const [isBulbOn, setIsBulbOn] = useState(true);
+  return (
+    <div>
+      <BulbState isBulbOn={isBulbOn} />
+      <ToggleBulbState setIsBulbOn={setIsBulbOn} />
+    </div>
+  );
+};
+
+// here we need isBulbOn
+const BulbState = ({ isBulbOn }) => {
+  return <div>{isBulbOn ? "Bulb is ON" : "Bulb is OFF"}</div>;
+};
+
+// here we need setIsBulbOn
+const ToggleBulbState = ({ setIsBulbOn }) => {
+  const checkStatus = () => {
+    // how this line of code is working
+    setIsBulbOn((currentState) => !currentState);
+  };
+
+  return (
+    <div>
+      <button onClick={checkStatus}> Toggle the Bulb </button>
+    </div>
+  );
+};
+
 export default App;
 
 /*
-useRef : reference to a value, such that when you change the value, the component DOES NOT RE-RENDER.
-
-if we use useState() hook to change the value then it leads to the re-render of the component.
-
-UseCase2 : Clock with start and stop functionality
--------------------------------------------------
-to start a clock we have to call setInterval method and for each second of time we will update.
-to stop the clock we need to call clearInterval method.
-
-        Catch : to keep the value of timer while stopping so that we can restart from the same moment.
-        a) use normal variable and store the value BUT it will not work because everytime state variable
-            called then re-render happens and it leads to the resetting the value of timer variable.
-
-        Q) what if click multiple time start button, why it is bahaving strange.
-
-we can use useState hook to store the value, but it leads to one extra re-render. just after clicking
-the start button, because we update the setTimer variable just after clicking the start button
-
-        Q) when we are console log the timer variable in the stopClock() funtion what is the value it 
-            it printing, kind of weird value it is.
-
-To avoid that extra re-rendering we can use useRef() ==> BEST METHOD
+when we need to pass variable to child component separately then in that case we need to rolling up the
+state, means instead of declaring the state variable under the child component we can declare that 
+variable in the LCA and from there with the help of props we pass the variable to the child.
 
 
 
 
 
 
-UseCase1 : From console try to move the cursor to the first text box when someone clicked on the submit button.
---------
-
-As we are doing it on console so we have to apply timeout of 3 sec.
-But if we go by the code then directly based on the ID we can manipulate the DOM element.
-
-window.setTimeout(function(){
-  document.getElementById("name").focus()
-}, 3000);
 
 
-it is not a good way to use getElementById() while using react, Better way is to use ref() hooks
-to refer the dom element
 
-ref attribute tells us to which dom element need to be taken care 
-and whatever the process we want to apply in the dom element we define using .current method.
-
+Initialize the fresh react project
+==================================
+1. npm create vite@Latest
+2. npm install
+3. npm run dev
 
 */
