f57f0d9453c414df9a38e438efaa77fa27ca6852
Rishi-Mohan <jharishimohan@gmail.com>
2025-02-03 22:00:59 +0530

    currentPage activeness and left and right corner with disable logic

--- Pagination-React/Pagination/src/App.jsx
+++ Pagination-React/Pagination/src/App.jsx
@@ -14,6 +14,7 @@ const ProductCard = ({ image, title }) => {
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState([0]);
 
   const fetchData = async () => {
     const data = await fetch("https://dummyjson.com/products?limit=500");
@@ -26,23 +27,61 @@ function App() {
   }, []);
 
   const total = products.length;
-  const cntOfPage = total / PAGE_SIZE;
+  const cntOfPage = Math.ceil(total / PAGE_SIZE);
+
+  console.log(cntOfPage);
+
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  const handlePageChange = (n) => {
+    setCurrentPage(n);
+  };
+
+  const moveToPrev = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const moveToNext = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
 
   return !products || products.length === 0 ? (
     <h1>No Products</h1>
   ) : (
     <div className="App">
-
       {/* logic for the pagination component : look and feel*/}
       <div className="pagination-container">
-        {[...Array(10).keys()].map((n) => (
-          <span className="page-number">{n}</span>
+        <button
+          disabled={currentPage === 0}
+          className={`page-number `}
+          onClick={() => moveToPrev()}
+        >
+          {" "}
+          ⬅️{" "}
+        </button>
+        {[...Array(cntOfPage).keys()].map((n) => (
+          <button
+            className={`page-number ${currentPage == n ? "active" : ""} `}
+            key={n}
+            onClick={() => handlePageChange(n)}
+          >
+            {n}
+          </button>
         ))}
+        <button
+          disabled={currentPage === cntOfPage - 1}
+          className="page-number"
+          onClick={() => moveToNext()}
+        >
+          {" "}
+          ➡️{" "}
+        </button>
       </div>
 
       {/* here we are loading all the cards */}
       <div className="container">
-        {products.map((p) => (
+        {products.slice(start, end).map((p) => (
           <ProductCard key={p.id} image={p.thumbnail} title={p.title} />
         ))}
       </div>
