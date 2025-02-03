55fa4c8800abbb386249c8862eeae5b521695d9d
Rishi-Mohan <jharishimohan@gmail.com>
2025-02-03 21:15:56 +0530

    pagination container desing, without logic

--- Pagination-React/Pagination/src/App.jsx
+++ Pagination-React/Pagination/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const PAGE_SIZE = 10;
+
 const ProductCard = ({ image, title }) => {
   return (
     <div className="product-card">
@@ -23,12 +25,22 @@ function App() {
     fetchData();
   }, []);
 
-  const val = products?.length;
+  const total = products.length;
+  const cntOfPage = total / PAGE_SIZE;
 
   return !products || products.length === 0 ? (
     <h1>No Products</h1>
   ) : (
     <div className="App">
+
+      {/* logic for the pagination component : look and feel*/}
+      <div className="pagination-container">
+        {[...Array(10).keys()].map((n) => (
+          <span className="page-number">{n}</span>
+        ))}
+      </div>
+
+      {/* here we are loading all the cards */}
       <div className="container">
         {products.map((p) => (
           <ProductCard key={p.id} image={p.thumbnail} title={p.title} />
